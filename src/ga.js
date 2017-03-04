89230c7814e6a8b6408cfdf541dfbffcb9e9d2fb
Tom Wey <tjmwey@gmail.com>
2017-03-04 19:36:47 +0000

    Track pageviews
    Including URL fragments.

--- src/ga.js
+++ src/ga.js
@@ -4,4 +4,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', process.env.GOOGLE_TRACKING_ID, 'auto');
-ga('send', 'pageview');
+ga('send', 'pageview', {
+ 'page': location.pathname + location.search  + location.hash
+});
